ebf2cb3dadea5adbc68dd46c4a8c5dbae923645c
Vitaliya Chumakova <chumakova@mindbox.ru>
2022-05-21 17:04:14 +0300

    update username

--- PluginName/PluginNamePlugin.cs
+++ PluginName/PluginNamePlugin.cs
@@ -11,22 +11,7 @@ namespace PluginName {
 	[UsedImplicitly]
 	public class PluginNamePlugin : IPlugin {
 		public void OnLoaded() {
-			Assembly assembly = Assembly.GetExecutingAssembly();
-			string repository = assembly
-				.GetCustomAttributes<AssemblyMetadataAttribute>()
-				.First(x => x.Key == "RepositoryUrl")
-				.Value ?? throw new InvalidOperationException(nameof(AssemblyMetadataAttribute));
-
-			const string GitSuffix = ".git";
-			int index = repository.IndexOf(GitSuffix, StringComparison.Ordinal);
-			if (index >= 0) {
-				repository = repository[..(index + 1)];
-			}
-
-			string company = assembly
-				.GetCustomAttribute<AssemblyCompanyAttribute>()?.Company ?? throw new InvalidOperationException(nameof(AssemblyCompanyAttribute));
-
-			ASF.ArchiLogger.LogGenericInfo(Name + " by " + company + " | Support & source code: " + repository);
+			ASF.ArchiLogger.LogGenericInfo(Name + " by ezhevita | Support & source code: https://github.com/ezhevita/" + Name);
 		}
 
 		public string Name => nameof(PluginName);
